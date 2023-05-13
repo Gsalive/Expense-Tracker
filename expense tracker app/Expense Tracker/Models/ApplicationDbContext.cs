f58c6c2ce16894da84aa35af8ec830e80685beb0
Gsalive <gsalive2002@gmail.com>
2023-05-13 14:03:12 +0530

    initial commit

---
+++ expense tracker app/Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+        //during frun time the  these 2  classes will be converted into table  whihc  will only  happen if both the classes were
+        //present in the framework 
+        //during migration the table will be named as provided here
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
